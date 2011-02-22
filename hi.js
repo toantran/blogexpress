3e75bc2ad9d8e0c75df0b00a272ec10bdd628575
Toan Tran <toantran@comcast.net>
2011-02-21 22:04:47 -0600

    added hi.js

---
+++ hi.js
@@ -0,0 +1,20 @@
+/**
+ * @author toantran
+ */
+var kiwi= require('kiwi')
+
+kiwi.require('express');
+require('express/plugins')
+
+configure(function(){
+  use(MethodOverride);
+  use(ContentLength);
+  use(Logger);
+  set('root', __dirname);
+})
+
+get('/', function(){
+  this.halt(200, "Hello World!");
+})
+
+run();
